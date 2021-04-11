0af7a8a0db53b6cc8c240e3e7b2235eaba2f0e38
Graham King <animandosolutions@gmail.com>
2021-04-11 09:13:04 +0100

    No fallback suffix

--- cdk/bin/ssr-react-app.ts
+++ cdk/bin/ssr-react-app.ts
@@ -4,7 +4,8 @@ import { AppStack } from '../lib/app-stack';
 
 const app = new cdk.App();
 
-const suffix = process.env.SUFFIX || 'default'
+const suffix = process.env.SUFFIX!
+
 new AppStack(app, `SsrReactAppStack-${suffix}`, {
   stackName: `SsrReactAppStack-${suffix}`,
   suffix,
