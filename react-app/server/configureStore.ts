409203b503a8b14343a6a3cbaa620d76496dc68f
Graham King <animandosolutions@gmail.com>
2021-04-11 07:03:05 +0100

    Clean-up code

--- react-app/server/configureStore.ts
+++ react-app/server/configureStore.ts
@@ -1,19 +1,10 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-// import thunkMiddleware from 'redux-thunk'
-
-// import monitorReducersEnhancer from './enhancers/monitorReducers'
-// import loggerMiddleware from './middleware/logger'
+import { createStore } from 'redux'
 import rootReducer from '../src/store/rootReducer'
 import { loadedUsers } from '../src/store/users'
 
-const configureStore = (/*preloadedState*/) => {
-  // const middlewares = [/*loggerMiddleware, thunkMiddleware*/]
-  // const middlewareEnhancer = applyMiddleware(...middlewares)
+const configureStore = () => {
 
-  // const enhancers = [middlewareEnhancer, /*monitorReducersEnhancer*/]
-  // const composedEnhancers = compose(...enhancers)
-
-  const store = createStore(rootReducer/*, preloadedState, composedEnhancers*/)
+  const store = createStore(rootReducer);
 
   store.dispatch(loadedUsers({'1': {id: '1', firstName: 'Graham', surname: 'King'}}))
 
